70992fbba2aebef53aef223a97ae05296086c0c0
Pedroguibas <pedroguibas123@gmail.com>
2025-11-11 23:50:46 -0300

    feat: cria pasta uploads/animais para inserir fotos de animais

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./db/connection.js";
 const port = 3000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Torna pasta uploads pública
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Rota raiz
 import rootRouter from "./routes/root.js";
@@ -24,10 +31,9 @@ import validatorRouter from "./routes/validatorRouter.js";
 app.use("/api/validators", validatorRouter);
 
 try {
-    await connectDB();
-    app.listen(port)
-    console.log("Servidor rodando na porta: " + port);
-}
-catch (error) {
-    console.log("Erro: " + error.message);
+  await connectDB();
+  app.listen(port);
+  console.log("Servidor rodando na porta: " + port);
+} catch (error) {
+  console.log("Erro: " + error.message);
 }
